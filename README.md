27b1c2468c41a8f738212bee2d1d75314dd2a448
Drew Harris <contac@grmnygrmny.com>
2012-09-25 09:17:42 -0400

    Intial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# drew_browser_test_2
+
+
+
+
+## Install
+
+
+     npm install drew_browser_test_2
+
+## Testing
+
+    git clone 
+    npm install
+    open test/index.html
---
+++ drew_browser_test_2.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("drew_browser_test_2", function(){
+    it("should have at least one test", function(){
+        assert.fail("Write a test!");
+    });
+});
